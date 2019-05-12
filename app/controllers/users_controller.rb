6db422dce7e30f52b0bc804a3c835acd208b2ab2
kpediad <pediadk@gmail.com>
2019-05-12 16:55:22 +0300

    Adds users#new action and users/new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :google_signup)
+  end
 end
