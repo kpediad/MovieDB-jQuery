b3818cb185b7ff7e25f390cedbfc2f772544da13
kpediad <pediadk@gmail.com>
2019-05-12 22:05:17 +0300

    Reorganizes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  get 'welcome/home'
-  get 'sessions/new', as: :login
-  get 'sessions/create'
-  get 'sessions/destroy'
+
+  get '/login', to: 'sessions#new'
+  delete '/logout', to: 'sessions#destroy'
+
+  get 'auth/:provider/callback', to: 'sessions#googleAuth'
+  get 'auth/failure', to: redirect('/login')
+
   resources :reviews
   resources :movies
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "welcome#home"
-
-  get 'auth/:provider/callback', to: 'sessions#googleAuth'
-  get 'auth/failure', to: redirect('/')
 end
