0ad898e3b9054c18870d1834763c1d054a308cc4
kpediad <pediadk@gmail.com>
2019-05-12 15:32:06 +0300

    Handles oauth 2.0 google signup/login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,5 +6,29 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out(current_user)
+    redirect_to root_path
+  end
+
+  def googleAuth
+    # Get access tokens from the google server
+    access_token = request.env["omniauth.auth"]
+    user = User.from_omniauth(access_token)
+    if user.save then
+      log_in(user)
+      redirect_to root_path
+    else
+      redirect_to login_path, alert: 'OAuth login error! Please try again.'
+    end
+  end
+
+  private
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out(user)
+    session.delete :user_id
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
   has_many :reviews
   has_many :movies, through: :reviews
+
+  def self.from_omniauth(auth)
+    # Creates a new user only if it doesn't exist
+    where(email: auth.info.email).first_or_initialize do |user|
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.google_signup = true
+      user.password = SecureRandom.hex
+    end
+  end
 end
