db8ee6875e914c426c4a5793d801af6efe438ec0
kpediad <pediadk@gmail.com>
2019-05-12 12:36:54 +0300

    Adds root route

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   resources :movies
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "welcome#home"
 end
