2379b4d409322098c57aabb2c5a53c9d5dc2e04a
kpediad <pediadk@gmail.com>
2019-05-12 17:17:32 +0300

    Adds users#edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,20 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      log_in(@user)
+      redirect_to root_path
+    else
+      redirect_to new_user_path
+    end
   end
 
   def edit
     @user = User.find(params[:id])
+    redirect_to root_path, alert: 'User does not exist!' if !@user
+    redirect_to root_path, alert: 'You cannot edit this user profile!' if @user != current_user
+    redirect_to login_path, alert: 'Please login first!' if !logged_in?  
   end
 
   def update
