64d4fdd90e235ce037b0ca37fcbe50210ef59bb3
kpediad <pediadk@gmail.com>
2019-05-12 14:01:14 +0300

    Adds oauth 2.0 google infrastructure

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "welcome#home"
+
+  get ‘auth/:provider/callback’, to: ‘sessions#googleAuth’
+  get ‘auth/failure’, to: redirect(‘/’)
 end
