a82925411df0431b2c3f72e4047a5a9ba11ae574
kpediad <pediadk@gmail.com>
2019-05-14 20:27:15 +0300

    Adds initial movie edit view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,7 @@ class MoviesController < ApplicationController
 
   def new
     @movie = Movie.new
+    @movie.reviews.build
   end
 
   def create
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
+  accepts_nested_attributes_for :reviews
 end
