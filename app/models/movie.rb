5185d22e370aa8ca9022b54be278ec7987360f83
kpediad <pediadk@gmail.com>
2019-05-14 21:44:14 +0300

    Adds initial validations for movie and review models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,7 @@ class Movie < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
   accepts_nested_attributes_for :reviews
+
+  validates :title, presence: true, uniqueness: { scope: :release_year, message: "exists with the same release year" }
+  validates :release_year, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :movie
+
+  validates :rating, :movie_id, :user_id, presence: true
+
 end
