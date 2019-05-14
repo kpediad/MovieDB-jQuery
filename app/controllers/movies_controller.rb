55fc80cde7d3e4263560b7b21b1be11d93235b23
kpediad <pediadk@gmail.com>
2019-05-14 19:17:16 +0300

    Adds actions edit show index update for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,9 +18,18 @@ class MoviesController < ApplicationController
   end
 
   def edit
+    @movie = Movie.find(params[:id])
   end
 
   def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params) then
+      flash.notice = "Movie details were updated successfully!"
+      redirect_to movie_path(@movie)
+    else
+      flash.now.alert = "#{@movie.error_msg}"
+      render :edit
+    end
   end
 
   def index
@@ -29,6 +38,12 @@ class MoviesController < ApplicationController
 
   def show
     @movie = Movie.find(params[:id])
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :release_year, :synopsis)
   end
 
 end
