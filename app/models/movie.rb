83cef9c46418e0b7de08d39086eb12cdb0d56115
kpediad <pediadk@gmail.com>
2019-05-14 22:24:59 +0300

    Adds validations for movie and review models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,5 +4,6 @@ class Movie < ApplicationRecord
   accepts_nested_attributes_for :reviews
 
   validates :title, presence: true, uniqueness: { scope: :release_year, message: "exists with the same release year" }
-  validates :release_year, presence: true
+  validates :release_year, presence: true, inclusion: { in: 1895..Time.now.year, message: "needs to be between 1895 and #{Time.now.year}" }
+  validates :synopsis, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,7 @@ class Review < ApplicationRecord
   belongs_to :movie
 
   validates :rating, :movie_id, :user_id, presence: true
+  validates :rating, inclusion: { in: [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0] }
+  validates :content, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
 
 end
