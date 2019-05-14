00dda9dbab6902792425981a94f20a318de8bfde
kpediad <pediadk@gmail.com>
2019-05-14 22:41:20 +0300

    Completes new movie form with optional review

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,10 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :movie
 
+  RATINGS = [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]
+
   validates :rating, :movie_id, :user_id, presence: true
-  validates :rating, inclusion: { in: [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0] }
+  validates :rating, inclusion: { in: RATINGS }
   validates :content, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
 
 end
