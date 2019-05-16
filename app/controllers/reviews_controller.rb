c8e9d3901f68ce1bd8ff2ba5d87b32e7b2881203
kpediad <pediadk@gmail.com>
2019-05-16 08:34:28 +0300

    Adds basic reviews controller structure

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,4 +8,26 @@ class ReviewsController < ApplicationController
     @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.find(params[:id])
   end
+
+  def new
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.build
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+  
 end
